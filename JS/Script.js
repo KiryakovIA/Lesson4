51039120d9c0a10e841f05305e64ab2a5468896a
KiryakovIA <48943805+KiryakovIA@users.noreply.github.com>
2019-04-04 10:31:42 +0300

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -1,1 +1,27 @@
-﻿
+﻿task1();
+
+
+
+function task1() {
+	console.log('Task 1')
+	let num = +prompt('Введите число от 0 до 999');
+	console.log(getObject(num));
+}
+
+
+function getObject(num) {
+	let res = {};
+	if (num >= 0 && num <= 999)
+	{
+		res.hundreds = Math.floor(num / 100);
+		num = num % 100;
+		res.tens = Math.floor(num / 10);
+		num = num % 10;
+		res.units =  num;
+	}
+	else
+	{
+		console.log('Ошибка. Введите число от 0 до 999');
+	}
+	return res;
+}
